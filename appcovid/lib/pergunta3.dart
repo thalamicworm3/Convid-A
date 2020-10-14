7ae17c0a87e3db5952a879008c0e0f1acfd8b38b
Álan Maida <alandematosmaida@hotmail.com>
2020-10-14 11:40:23 -0300

    alteracao pergunta 01

---
+++ appcovid/lib/pergunta3.dart
@@ -0,0 +1,153 @@
+import 'package:appcovid/respostafim.dart';
+import 'package:appcovid/respostatela3.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:appcovid/menu.dart';
+import 'package:appcovid/respostatela1.dart';
+
+String opcao = 'Tem covid';
+
+class Pergunta3 extends StatelessWidget {
+  Pergunta3();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('3ª Pergunta'),
+          backgroundColor: cor,
+        ),
+        body: new Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                "Mora ou trabalha com alguém que apresentou diagnóstico para Covid-19, nos últimos 15 dias:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: MyStatefulWidget()),
+            new Expanded(
+                child: ListView.builder(
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return Container(
+                            margin: const EdgeInsets.all(15.0),
+                            padding: const EdgeInsets.all(3.0),
+                            //color: Colors.blueGrey,
+                            decoration: BoxDecoration(
+                                color: cor,
+                                border: Border.all(color: Colors.black)),
+                            child: ListTile(
+                              title: Text(
+                                "Enviar Resposta",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              onTap: () {
+                                if (opcao == 'Sim') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Respostatela3()),
+                                  );
+                                  opcao = 'Sim';
+                                } else {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Respostafim()),
+                                  );
+                                  opcao = 'Sim';
+                                }
+                              },
+                            ));
+                      }else{
+                        if(index==1){
+                          return Container(
+                            margin: const EdgeInsets.all(15.0),
+                            padding: const EdgeInsets.all(3.0),
+                            //color: Colors.blueGrey,
+                            decoration: BoxDecoration(
+                                color: cor,
+                                border: Border.all(color: Colors.black)),
+                            child: ListTile(
+                              title: Text(
+                                "Volta ao Menu",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              onTap: () {                                
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => MenuScreen()),
+                                  );
+                                  opcao = 'Sim';                                
+                              },
+                            ));
+
+                        }
+                      }
+                    })),
+          ],
+        ));
+  }
+}
+
+enum SingingCharacter { op1, op2 }
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  SingingCharacter _character = SingingCharacter.op1;
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: const Text('Sim'),
+          leading: Radio(
+            value: SingingCharacter.op1,
+            groupValue: _character,
+            onChanged: (SingingCharacter value) {
+              setState(() {
+                _character = value;
+                print(_character);
+                opcao = 'Sim';
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: const Text('Não'),
+          leading: Radio(
+            value: SingingCharacter.op2,
+            groupValue: _character,
+            onChanged: (SingingCharacter value) {
+              setState(() {
+                _character = value;
+                print(_character);
+                opcao = 'Não';
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ appcovid/lib/respostafim.dart
@@ -0,0 +1,68 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:appcovid/menu.dart';
+
+class Respostafim extends StatelessWidget {
+  Respostafim();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Resposta: Covid Netativo'),
+          backgroundColor: cor,
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Image.asset(
+              'assets/images/covidpositivo.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 5, bottom: 25),
+                padding: const EdgeInsets.all(3.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      bottomRight: Radius.circular(15)),
+                ),
+                child: Text(
+                  "De acordo com suas respostas enviadas, percebemos que está saudável no momento e apto a voltar as aulas. Tome todos os cuidados necessários, para evitar a proliferação do vírus.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            ButtonTheme(
+              height: 50,
+              minWidth: 250,
+              buttonColor: Colors.blue,
+              child: RaisedButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0)),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MenuScreen()),
+                  );
+                },
+                child: Text(
+                  "Finalizar Consulta",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
